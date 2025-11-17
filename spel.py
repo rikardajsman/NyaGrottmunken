974b4b2c61f89e5c2dbf4b0476bfc4afcb50340a
Rikard Ajsman <rikard.ajsman@skola.taby.se>
2025-11-17 12:22:29 +0100

    Lagt till frågor

--- spel.py
+++ spel.py
@@ -1,1 +1,15 @@
 print("Hej spelet!")
+
+
+print("Grottmunkenspelet")
+print("")
+
+score = 0
+
+print("1. Vad är 2+7?")
+answer1 = input("Ange ditt svar ")
+
+if answer1 == "9":
+    score += 1
+
+print("Du fick " + str(score +"/1"))
