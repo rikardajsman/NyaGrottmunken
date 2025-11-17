10c62b193a3a4278926bbb7d72e46746ec5d9e58
Hugo Rydback <hugo.rydback@skola.taby.se>
2025-11-17 12:14:33 +0100

    first commit

--- spel.py
+++ spel.py
@@ -1,1 +1,5 @@
-print("Hej spelet!")
+def intro():
+    print("* Välkommen till grottan, munk. *")
+    print("Du vaknar ensam i mörkret och måste klura dig ut…\n")
+    print("Du har 3 liv. Misslyckas du tre gånger skickas du tillbaka till början.\n")
+    input("Tryck ENTER för att börja...")
