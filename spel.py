be8c78ba0338251a3fdd8cfafa1eab22f11fec1b
Rikard Ajsman <rikard.ajsman@skola.taby.se>
2025-11-17 12:25:16 +0100

    Ändrat ordningsföljd på utskrifterna

--- spel.py
+++ spel.py
@@ -1,5 +1,8 @@
 print("Hej spelet!")
-
+print("* Välkommen till grottan, munk. *")
+print("Du vaknar ensam i mörkret och måste klura dig ut…\n")
+print("Du har 3 liv. Misslyckas du tre gånger skickas du tillbaka till början.\n")
+input("Tryck ENTER för att börja...")
 
 print("Grottmunkenspelet")
 print("")
@@ -13,8 +16,4 @@ if answer1 == "9":
     score += 1
 
 print("Du fick " + str(score +"/1"))
-def intro():
-    print("* Välkommen till grottan, munk. *")
-    print("Du vaknar ensam i mörkret och måste klura dig ut…\n")
-    print("Du har 3 liv. Misslyckas du tre gånger skickas du tillbaka till början.\n")
-    input("Tryck ENTER för att börja...")
+
